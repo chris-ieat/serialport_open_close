507e15a579c6845f939aada7bb35ee70b5f0ec45
ChrisTakahashi-ieat <takahashi@ieat-fresh.com>
2021-02-09 00:52:20 +0900

    first commit

---
+++ serial_reset.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python 
+import serial
+import time
+import sys
+
+if __name__ == '__main__':
+
+    argv = sys.argv
+    if len(argv) != 2:
+        print("usage:\n python {} DEVICE".format(argv[0]))
+        sys.exit()
+    
+    device = argv[1]
+
+    print("{}".format(device))
+    ser = serial.Serial(device,115200,timeout=None)
+
+    ser.dtr=False
+    time.sleep(1)
+
+    ser.close()
